2a99092c873693bcb9e8681d804cc8834e777629
ariafatah0711 <ariafatah0711@gmail.com>
2024-01-01 11:25:41 +0700

    update lint

--- src/script/data/request.js
+++ src/script/data/request.js
@@ -1,42 +1,42 @@
-import axios from "axios";
+import axios from 'axios'
 
 const searchRequest = (keyword, type) => {
   const queryParams = {
     q: keyword,
-    sfw: true,
-  };
+    sfw: true
+  }
 
   return axios
     .get(`https://api.jikan.moe/v4/${type}`, { params: queryParams })
     .then((response) => {
       if (response.data.data) {
-        return Promise.resolve(response.data.data);
+        return Promise.resolve(response.data.data)
       } else {
         return Promise.reject(
           new Error(`top ${type} is not found (too many requests!)`)
-        );
+        )
       }
     })
     .catch((error) => {
-      console.warn(error);
-    });
-};
+      console.warn(error)
+    })
+}
 
 const topRequest = (type) => {
   return axios
     .get(`https://api.jikan.moe/v4/top/${type}`)
     .then((response) => {
       if (response.data.data) {
-        return Promise.resolve(response.data.data);
+        return Promise.resolve(response.data.data)
       } else {
         return Promise.reject(
           new Error(`top ${type} is not found (too many requests!)`)
-        );
+        )
       }
     })
     .catch((error) => {
-      console.warn(error);
-    });
-};
+      console.warn(error)
+    })
+}
 
-export { searchRequest, topRequest };
+export { searchRequest, topRequest }
